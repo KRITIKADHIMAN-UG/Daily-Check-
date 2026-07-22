e83871fd8c4966667946861257cceac9a7f40c3d
Kritika Dhiman <kritikadhiman1492006@gmail.com>
2026-07-22 08:20:13 +0530

    Time: 0 ms (100%), Space: 25.2 MB (33.12%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+
+        for (auto& it : mp) {
+            if (it.second > 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
